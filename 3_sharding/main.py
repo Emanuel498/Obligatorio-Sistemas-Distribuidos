92102c6d8cb0bb9100c952c9e86351105ae87ff1
Emaonide <ef1310@hotmail.com>
2023-05-08 23:35:36 -0300

    Funcionando el endpoint de measure y enviar a la cola

--- 3_sharding/main.py
+++ 3_sharding/main.py
@@ -1,49 +1,53 @@
+#!/usr/bin/env python
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 import uvicorn
 import pika
+import os
+
+QUEUE_HOST = os.getenv('QUEUE_HOST', 'localhost')
+QUEUE_NAME = os.getenv('QUEUE_NAME', 'test')
 
 class Alert(BaseModel):
     name: str
     flow: float
     location: str
 
-credentials = pika.PlainCredentials('guest', 'guest')
+MESSAGES=[
+    "TEST 1",
+    "TEST 2",
+    "TEST 3",
+    "TEST 4",
+    "TEST 5"
+]
 
-def create_queue(queue_name: str):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1', port=5672, credentials=credentials))
-    # ¿Para que creamos otro canal, si el canal ya existe y fue con el que llamamos?
+def create_queue():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=QUEUE_HOST, port=5672))
     channel = connection.channel()
-    # Declarar cola, crear si es necesario. Este método crea o comprueba una cola.
-    channel.queue_declare(queue=queue_name)
+    #Declaramos la cola que va a utiliar
+    channel.queue_declare(queue=QUEUE_NAME) 
+    
+    for message in MESSAGES:
+        #Publico los mensajes en la cola
+        channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
+        print("[x] Mensaje enviado: %s " % message)
+    
     connection.close()
-
-def publish_message_to_rabbitmq(queue_name: str, message: str):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1', port=5672, credentials=credentials))
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name)
-    channel.basic_publish(exchange='', routing_key=queue_name, body=message)
-    connection.close()
-
-def consume_messages_from_rabbitmq(queue_name: str):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1', port=5672, credentials=credentials))
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name)
-    # el auto_ack es "Dígale al corredor que no espere una respuesta". ¿Por qué espero una respuesta?
-    method_frame, header_frame, body = channel.basic_get(queue=queue_name, auto_ack=True)
-    if method_frame:
-        print("Mensaje recibido: ", body)
-    else:
-        print("No hay mensajes en la cola.")
-    connection.close()
-
 
 app = FastAPI()
 
 endpoints = ["http://localhost:8000", "http://localhost:8001"]
 
 endpoint_flag = True
+
+@app.get("/measure")
+def measure():
+    try:
+        create_queue()
+    except Exception as e:
+        print(e)
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 def balance_endpoints():
     global endpoint_flag
@@ -53,35 +57,6 @@ def balance_endpoints():
     endpoint_flag = True
     return endpoints[1]
 
-@app.post("/enqueue")
-def post_message_to_rabbitmq():
-    try:
-        publish_message_to_rabbitmq("alertas", "alerta")
-        return {"message": "Mensaje enviado correctamente a la cola."}
-    except Exception as e:
-        print(e)
-        return JSONResponse(content={"error": str(e)}, status_code=500)
-
-@app.post("/measure")
-def measure():
-    try:
-        return JSONResponse(content={"message": consume_messages_from_rabbitmq("alertas")}, status_code=200)
-    except Exception as e:
-        print(e)
-        return JSONResponse(content={"error": str(e)}, status_code=500)
-
 if __name__ == '__main__':
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    # Creamos un canal para comunicarnos con RabbitMQ
-    channel = connection.channel()
 
-    # ¿Esto qué hace? ¿Para que hacemos esto?
-    channel.queue_declare(queue='mi_cola')
-
-    # Publica en el canal con el intercambio, la clave de enrutamiento y el cuerpo proporcionados.
-    # ¿Qué hacen los parámetros?
-    channel.basic_publish(exchange='', routing_key='mi_cola', body='Hola, mundo!')
-    print("Mensaje enviado")
-
-    connection.close()
     uvicorn.run(app, host="0.0.0.0", port=80)
