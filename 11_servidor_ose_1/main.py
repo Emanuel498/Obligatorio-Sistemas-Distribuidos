c6ac96ed0824cdebf489cd98fa9a50dd0b80e2d1
Emaonide <ef1310@hotmail.com>
2023-05-20 17:52:37 -0300

    Se corrige las queue y servidor de ose consume de ambas

--- 11_servidor_ose_1/main.py
+++ 11_servidor_ose_1/main.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python
-import pika, sys, os
+import pika, sys, os, threading
 
 QUEUE_NAME = os.getenv('QUEUE_NAME', 'test_queue')
 ALERTS_QUEUE_PRIMARY = os.getenv('ALERTS_QUEUE_PRIMARY', 'alerts-queue-1')
 ALERTS_QUEUE_SECONDARY = os.getenv('ALERTS_QUEUE_SECONDARY', 'alerts-queue-2')
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=ALERTS_QUEUE_PRIMARY, port=5672))
-    print('Iniciando')
+def main(queueName, host):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host, port=5672))
+    
     channel = connection.channel()
-
-    channel.queue_declare(queue=QUEUE_NAME)
+    
+    channel.queue_declare(queue=queueName)
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue=QUEUE_NAME, on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue=queueName, on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
 if __name__ == '__main__':
-    print('Iniciando')
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+    thread1 = threading.Thread(target=main, args=(QUEUE_NAME, ALERTS_QUEUE_PRIMARY))
+    thread2 = threading.Thread(target=main, args=(QUEUE_NAME, ALERTS_QUEUE_SECONDARY))
+    
+    thread1.start()
+    thread2.start()
+    
+    thread1.join()
+    thread2.join()
