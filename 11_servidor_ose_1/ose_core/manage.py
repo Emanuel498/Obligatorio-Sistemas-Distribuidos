954f279dc5afc8240b5edb0c1c97f3c3d5736605
Federico <federico.alberti.gurnerio@gmail.com>
2023-05-22 16:45:41 -0300

    Fix

--- 11_servidor_ose_1/ose_core/manage.py
+++ 11_servidor_ose_1/ose_core/manage.py
@@ -16,8 +16,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
     subprocess.call(["python", "../main.py"])
+    execute_from_command_line(sys.argv)
+    
 
 
 if __name__ == '__main__':
