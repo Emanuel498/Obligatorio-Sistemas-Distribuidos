bb3258c914b9430cb395d47b958a7e55ab307bfd
Federico <federico.alberti.gurnerio@gmail.com>
2023-05-22 21:35:40 -0300

    django consuming queues

--- 11_servidor_ose_1/main.py
+++ 11_servidor_ose_1/main.py
@@ -21,7 +21,6 @@ def main(queueName, host):
     channel.start_consuming()
 
 if __name__ == '__main__':
-    print('Test')
     thread1 = threading.Thread(target=main, args=(QUEUE_NAME, ALERTS_QUEUE_PRIMARY))
     thread2 = threading.Thread(target=main, args=(QUEUE_NAME, ALERTS_QUEUE_SECONDARY))
     
