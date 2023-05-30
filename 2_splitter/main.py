2964ad5b7dbf4da30d4bbca0d4c5f9abf028285a
Emaonide <ef1310@hotmail.com>
2023-05-30 00:11:12 -0300

    Se añade lógica en el splitter y se agrega splitter al dockercompose.yml

--- 2_splitter/main.py
+++ 2_splitter/main.py
@@ -1,20 +1,45 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+import json
 import requests
 import uvicorn
 
+# Clase que simula los datos que van a ser enviados por sensores.
 class Measure(BaseModel):
     name: str
     flow: float
     location: str
 
-
+# Difine la app
 app = FastAPI()
 
+"""
+Endpoint que recibe una medida y ... .
+
+Attributes:
+    measure (Measure): Medidas tomadas por los sensores.
+"""
 @app.post("/measure")
-def ping(measure: Measure):
-    return measure
+def postMeasure(measure: Measure):
+    """
+        Si la medida de flujo de agua, reportado por el sensor, 
+        es menor a 12.8 litros se considera como alerta.
+    """
+    if(measure.flow < 12.8):
+        # Enviamos la medida al endpoint del Spliter del alert
+        response = requests.post("http://producer-alerts/sendAlert", json.dumps(measure.__dict__))
+        return JSONResponse(
+            status_code=200,
+            content="Se envio alerta al Splitter de alertas.",
+        )
+    else:
+        # Enviamos el dato al endpoint del Spliter del datos.
+        response = requests.post("http://producer-data/sendData", json.dumps(measure.__dict__))
+        return JSONResponse(
+            status_code=200,
+            content="Se envio dato al Splitter de datos.",
+        )
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=80)
