b51b39b0ee9a539f80498dc63eb7d67134a00c94
Emaonide <ef1310@hotmail.com>
2023-06-15 20:48:22 -0300

    No se creaba así el 2do servidor
    Se debe crear con comandos desde terminal

--- 14_servidor_ine_1/ine_core/ine_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OseAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ine_app'
--- 14_servidor_ine_1/ine_core/ine_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    print('HOLA')
-    return HttpResponse("Hello, world.")
-
-
