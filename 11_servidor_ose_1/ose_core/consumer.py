262ed4f8904dc5a2e2ccd189c7da1186064936b6
Diego <diego2737@gmail.com>
2023-06-25 18:52:25 -0300

    Fixes

--- 11_servidor_ose_1/ose_core/consumer.py
+++ 11_servidor_ose_1/ose_core/consumer.py
@@ -11,16 +11,16 @@ QUEUE_DATA = os.getenv('QUEUE_DATA', 'data_queue')
 DATA_QUEUE_PRIMARY = os.getenv('DATA_QUEUE_PRIMARY', 'data-queue-1')
 DATA_QUEUE_SECONDARY = os.getenv('DATA_QUEUE_SECONDARY', 'data-queue-2')
 
-
-
 def populate(queueName, host, model):
-    print('consumiendo')
+    print(f'Starting consumer for queue {queueName} host {host} and model {model}')
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=host, port=5672))
     channel = connection.channel()
     channel.queue_declare(queue=queueName)
 
     def callback(ch, method, properties, body):
+        print('consumiendo')
         data = json.loads(body)
+        print('data consumida', data)
         print(data['name'])
         model.objects.create(name=data['name'], flow=data['flow'], location=data['location'])
 
@@ -31,7 +31,7 @@ def populate(queueName, host, model):
 if __name__ == '__main__':
     thread1 = threading.Thread(target=populate, args=(QUEUE_ALERT, ALERTS_QUEUE_PRIMARY, Alerts))
     thread2 = threading.Thread(target=populate, args=(QUEUE_ALERT, ALERTS_QUEUE_SECONDARY, Alerts))
-    thread3 = threading.Thread(target=populate, args=(QUEUE_DATA, DATA_QUEUE_SECONDARY, Data))
+    thread3 = threading.Thread(target=populate, args=(QUEUE_DATA, DATA_QUEUE_PRIMARY, Data))
     thread4 = threading.Thread(target=populate, args=(QUEUE_DATA, DATA_QUEUE_SECONDARY, Data))
     
     thread1.start()
